1cc07444c90f82944f9b4e36002568ad6700e5b7
Ben Speakman <b3n5p34km4n@gmail.com>
2019-04-15 00:33:38 -0400

    basic shopping cart

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,8 @@
 class Product < ApplicationRecord
+  has_many :line_items
+
+  before_destroy :ensure_not_referenced_by_line_item
+
   validates :name, :description, presence: true
   validates :name, uniqueness: true
   validates :price, numericality: { greater_than_or_equal_to: 50 }
@@ -6,4 +10,14 @@ class Product < ApplicationRecord
   def price_in_dollars
     price.to_f / 100
   end
+
+  private
+
+  def ensure_not_referenced_by_line_item
+    unless line_items.empty?
+      errors.add(:base, "This item is in a cart")
+      throw :abort
+    end
+  end
+
 end
--- test/controllers/products_controller_test.rb
+++ test/controllers/products_controller_test.rb
@@ -46,4 +46,12 @@ class ProductsControllerTest < ActionDispatch::IntegrationTest
 
     assert_redirected_to products_url
   end
+
+  test 'a product in a cart cannot be deleted' do
+    assert_difference('Product.count', 0) do
+      delete product_url(products(:two))
+    end
+
+    assert_redirected_to products_url
+  end
 end
