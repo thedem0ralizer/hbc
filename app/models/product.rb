a0ca7ea757c4b95d16acc9f4129d5778f9fa716b
Ben Speakman <b3n5p34km4n@gmail.com>
2019-04-14 23:49:52 -0400

    display catalog

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,4 +2,8 @@ class Product < ApplicationRecord
   validates :name, :description, presence: true
   validates :name, uniqueness: true
   validates :price, numericality: { greater_than_or_equal_to: 50 }
+
+  def price_in_dollars
+    price.to_f / 100
+  end
 end
